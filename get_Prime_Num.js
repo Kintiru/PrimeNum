f231eef23adfc63c3ed8776c71cb3f9257ab8ee8
junee <junee1163@gmail.com>
2016-12-17 19:58:38 +0900

    Update get_Prime_Num.js

--- get_Prime_Num.js
+++ get_Prime_Num.js
@@ -1,44 +1,45 @@
 function getPrime_Num(startNum,endNum) {
-  var e;
-  var prime_Num = [];
-  var not_prime_Num = [];
-  for(;startNum =< endNum;startNum++){
-    for(var i = 2,i < startNum;i++){
-      e = startNum / i;
-      if(startNum < 1){
+  var checkP;
+  try{
+    if(startNum > endNum){ throw 'err1'}
+
+    if(isNaN(startNum) === true) {throw 'err2'}
+
+    if(isNaN(endNum) === true) {throw 'err3'}
+
+    if(startNum < 2){throw 'err4'}
+  }
+  catch(e){
+    if(e == 'err1') {return console.log('에러: 끝나는 숫자가 시작숫자보다 크면 안됍니다.');}
+
+    if(e == 'err2') {return console.log('에러: 시작숫자가 숫자가 아닙니다!');}
+
+    if(e == 'err3') {return console.log('에러: 끝나는 숫자가 숫자가 아닙니다!');}
+
+    if(e == 'err4') {startNum = 2;}
+  }
+  
+  for(;startNum <= endNum;startNum++){
+  var primeplus = 2;
+    for(var i = 2;i < startNum;i++){
+      checkP = startNum % i;
+      if(startNum < 2){
         break;
       }
       else if(startNum === 2){
         console.log('2')
+        break;
       }
-      else if(e === 0){
-        not_prime_Num.push(startNum);
+      else if(checkP === 0){
+        break;
       }
-      else{
-        prime_Num.push(startNum);
+      else if (checkP > 0) {
+        primeplus++;
+        break;
       }
+    }
+    if(primeplus == startNum) {
+      console.log(startNum);
     }
   }
-  console.log('솟수:\n');
-
-  prime_Num.forEach((item,index) =>{
-    if(index < endNum - 1){
-      console.log(item + ',');
-    }
-    else{
-      console.log(item);
-      break;
-    }
-  }));
-
-  console.log('합성수:\n');
-  not_prime_Num.forEach((item,index) =>{
-    if(index < endNum - 1){
-      console.log(item + ',');
-    }
-    else{
-      console.log(item);
-      break;
-    }
-  }));
 }
